f2390d2fbaeeffca88f98346c55f768942875311
Ayush Kumar <kumarayush9651@tutanota.com>
2025-12-04 19:58:39 +0530

    Added notes for refrence l r

---
+++ refrences_l_r.cpp
@@ -0,0 +1,24 @@
+// Arefrence is an alias for an object, is usually implemented
+// to hold a machine address of an object
+
+template<class T>
+class vector{
+    T* elem;
+
+    public:
+        T& operator[](int i){       //return reference to an element
+            return elem[i];
+        }
+
+        const T& operator[](int i)const {   //return reference to an const element
+            return elem[i];
+        }
+
+        void push_back(const T& a);
+};
+
+void f(const vector<double>& v){
+    double d1 = v[1];
+    v[2] = 7;
+    v.push_back(d1);
+}
